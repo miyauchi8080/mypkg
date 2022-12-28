d9006a36d62f279912c7568202b10e104844943a
kojimiyauchi <s21c1121uz@s.chibakoudai.jp>
2022-12-28 15:19:58 +0900

    commit

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,8 +5,8 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 def cb(msg):
-        global node
-        node.get_logger().info("Listen: %d" % msg.data)
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
 rclpy.init()
 node = Node("listener")
